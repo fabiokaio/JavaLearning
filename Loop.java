4bf9373fc233619cced0d0e4d3737dd86392af13
Fábio Kaio <fabiokaio501@gmail.com>
2025-10-13 14:56:42 +0000

    criando loops básicos

---
+++ Loop.java
@@ -0,0 +1,10 @@
+
+public class Loop {
+    public static void main (String[]args){
+        System.out.println("Contando de um até 100 ...");
+        for (int i = 0;i < 100; i++){
+            System.out.printf("%d...%n",i);
+        }   
+
+    }    
+}
